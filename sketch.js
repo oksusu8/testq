91ba7326f75384250e26aa757f202974effa5ff3
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-12-05 15:23:59 +0900

    1st
    fullscreenTest

---
+++ sketch.js
@@ -0,0 +1,30 @@
+let home;
+
+
+function preload() {
+  home = loadImage("home.png");
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  // fullscreen 모드로 전환을 위한 버튼 생성
+  let fullscreenButton = createButton('');
+  fullscreenButton.class('fullscreenBtn'); // 추가
+  fullscreenButton.position(0, height*0.45);
+  fullscreenButton.size(width, height*0.12);
+  fullscreenButton.mousePressed(triggerFullscreen);
+}
+
+function draw() {
+  background(220);
+   image(home,0, 0,windowWidth, windowHeight);
+  
+  // 그리기 코드 추가
+}
+
+// fullscreen 모드로 전환하는 함수
+function triggerFullscreen() {
+  fullscreen(true);
+}
