53896bf563edb36b014c567064548dde80d06c40
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-12-08 00:29:41 +0900

    10th

--- sketch.js
+++ sketch.js
@@ -1,67 +1,24 @@
-let buttonX, buttonY;
-
 let video;
-
-let shutterBtn;
-let autoBtn;
-let poseBtn;
-
-let isVisible = true;
+let capturedImage;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  buttonX = width / 2;
-  buttonY = height / 2;
-
   video = createCapture(VIDEO);
   video.size(width, height * 0.66);
   video.hide();
-  layoutDraw();
-
-  rectMode(CENTER);
-
-  let autoBtn = select('#autoBtn');
-  let captureBtn = select('#captureBtn');
-  let poseBtn = select('#poseBtn');
-  let screenBtn = select('#screenBtn');
-  //screenBtn.mousePressed(toggleFullscreen);
   
-  /*
-  shutterBtn = createButton('');
-  shutterBtn.class('shutterBtn');
-  shutterBtn.position(buttonX - (height * 0.12) / 2, height * 0.88 - (height * 0.12) / 2);
-  shutterBtn.size(height * 0.12, height * 0.12);
+  captureBtn = select('#captureBtn');
   
-  autoBtn = createButton('Auto');
-  autoBtn.class('autoBtn');
-  autoBtn.position(width * 0.91, height * 0.68);
-  autoBtn.size(height * 0.05, height * 0.05);
-  
-  poseBtn = createButton('POSE');
-  poseBtn.class('poseBtn');
-  poseBtn.position(width - (width * 0.2), height * 0.88 - (height * 0.09) / 2);
-  poseBtn.size(height * 0.11, height * 0.1);*/
-
+  captureBtn.mousePressed(talk);
   
 }
 
 function draw() {
-  image(video, 0, height * 0.04);
+  background(0);
+  image(video, 0, height * 0.08);
+  
 }
 
-function layoutDraw() {
-  fill(255);
-  noStroke();
-  rect(0, 0, width, height * 0.04);
-
-  fill(0);
-  noStroke();
-  rect(0, height * 0.7, width, height * 0.3);
+function talk() {
+  console.log('촬영');
 }
-
-function toggleFullscreen() {
-    let fs = fullscreen();
-    fullscreen(!fs);
-}
-
-
